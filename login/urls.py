b6b3895c7ad7352efd9eb4fd9cb8d9d9d4d92b87
vishal <cicada341@gmail.com>
2022-02-03 20:51:02 +0530

    First commit

---
+++ login/urls.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from . import views
+from django.urls import URLPattern, include, path
+
+urlpatterns = [
+    path('login/', views.login),
+    path('', views.login),
+    path('signup/', views.signup),
+    path('saveuser/', views.saveuser, name='saveuser'),
+    path('auth/', views.auth, name='auth'),
+]
---
+++ login/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import userdata
+# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def saveuser(request):
+    if request.method == 'POST':
+        user = request.POST.get('username')
+        pas = request.POST.get('password')
+        da = userdata(user=user, password=pas)
+        da.save()
+    count = dir(userdata)
+    nxt = userdata.objects.all()
+    all_user=''
+    for i in nxt:
+        all_user += ' '+i.user
+        if i.user == 'vishal':
+            exi = 'user found'
+            break
+        else:
+            exi = 'user not found'
+    context = {
+        'user_count' : count,
+        'nxt_data' : nxt,
+        'exi' : exi,
+    }
+    return render(request, 'saveuser.html', context=context)
+
+def auth(request):
+    if request.method == 'POST':
+        user = request.POST.get('user')
+        pas = request.POST.get('password')
+    usr = userdata.objects.all()
+    found = False
+    for i in usr:
+        if user == i.user and pas == i.password:
+            found = True
+            break
+    if found == True:
+        return render(request, 'loggedin.html')
+    else:
+        return render(request, 'wrong.html')
