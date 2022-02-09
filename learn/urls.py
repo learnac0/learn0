91a070c953ede8ac9bf7a06967d2ba5baeb6e7da
learnac0 <98963231+learnac0@users.noreply.github.com>
2022-02-09 17:55:09 +0530

    aa

--- learn/urls.py
+++ learn/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
